e83a3c4b99881806edba74dd16443e9467f4beb9
Lucas Moraes <lucasdemoraesc@gmail.com>
2021-03-05 02:51:32 -0300

    Topbar responsiva

--- src/app/components/templates/topbar/topbar.component.ts
+++ src/app/components/templates/topbar/topbar.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ShowSidebarService } from './../sidebar/event/show-sidebar.service';
 import { TopbarTitleService } from './../../../services/topbar-title.service';
+import { IsMobileService } from '../utils/is-mobile.service';
+import { MenuItem } from 'primeng/api';
 
 @Component({
   selector: 'app-topbar',
@@ -9,9 +11,23 @@ import { TopbarTitleService } from './../../../services/topbar-title.service';
 })
 export class TopbarComponent implements OnInit {
 
-  constructor(private showSidebarService: ShowSidebarService, private topbarTitleService: TopbarTitleService) { }
+  isMobile: Boolean;
+  items: MenuItem[];
 
-  ngOnInit(): void { }
+  constructor(private showSidebarService: ShowSidebarService, private topbarTitleService: TopbarTitleService, private isMobileService: IsMobileService) {
+    this.isMobile = isMobileService.checkPlatform();
+  }
+
+  ngOnInit(): void {
+    this.items = [
+      { label: 'Produtos', icon: 'fas fa-box', routerLink: '/sistema/produtos' },
+      { label: 'Estoque', icon: 'fas fa-cubes', routerLink: '/sistema/estoque' },
+      { label: 'Funcionários', icon: 'fas fa-address-card', routerLink: '/sistema/funcionarios' },
+      { label: 'Clientes', icon: 'fas fa-address-book', routerLink: '/sistema/clientes' },
+      { label: 'Marcas', icon: 'fas fa-table', routerLink: '/sistema/marcas' },
+      { label: 'Categorias', icon: 'fas fa-tags', routerLink: '/sistema/categorias' }
+    ];
+  }
 
   get title(): string {
     return this.topbarTitleService.topbarData.title;
